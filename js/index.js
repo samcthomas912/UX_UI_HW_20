90e59e441289238dffbe345e32c6cfb5b01808e7
Sam Thomas <59207827+samcthomas912@users.noreply.github.com>
2020-06-15 15:35:47 -0400

    Scroll Button
    Fixed the scroll to top button

--- js/index.js
+++ js/index.js
@@ -99,7 +99,7 @@ $(function() {
   })
 
 
-  //Get the button:
+//Get the button:
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -111,5 +111,11 @@ function scrollFunction() {
   } else {
     mybutton.style.display = "none";
   }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
